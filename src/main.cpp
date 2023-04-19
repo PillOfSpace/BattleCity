0d738a1033f516c83c7f8bf5399d72393ba2456a
PillOfSpace <ennokey008@gmail.com>
2023-04-19 16:24:35 +0300

    OpenGL and glfw initilization

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,53 @@
 #include<glad/glad.h>
 #include <GLFW/glfw3.h>
 
+int g_WindowSizeX = 640;
+int g_WindowSizeY = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int wight, int height) {
+    g_WindowSizeX = wight;
+    g_WindowSizeY = height;
+    glViewport(0, 0, g_WindowSizeX, g_WindowSizeY); //Показываем в какой области мы хотим рисовать 
+}
+//закрытие окна по нажатию клавиши escape
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);//указываем GL что мы закрываем программу
+    }
+}
+
 int main(void)
 {
-    GLFWwindow* window;
+    GLFWwindow* pWindow;
 
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "glfwInit failed!" << std::endl;
         return -1;
+    }
+    //Указываю версию, если она меньше 4 и 6, то окно не запустится
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    //использую "кор про файл"
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    pWindow = glfwCreateWindow(g_WindowSizeX, g_WindowSizeY, "BattleCity", nullptr, nullptr);
+    if (!pWindow)
     {
+        std::cout << "glfwCreateWindow failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
+    glfwSetKeyCallback(pWindow, glfwKeyCallback);
+
+    /* делаем это окно текущим */
+    glfwMakeContextCurrent(pWindow);
 
     if (!gladLoadGL())
     {
@@ -27,19 +56,21 @@ int main(void)
         return -1;
     }
 
+    std::cout << "Render: " << glGetString(GL_RENDERER) << std::endl; // показывает название видеокарты
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl; //показыввает версию OpenGL
     std::cout << "OpenJL" << GLVersion.major << "." << GLVersion.minor << std::endl;
 
     glClearColor(0, 1, 0, 1);
 
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
