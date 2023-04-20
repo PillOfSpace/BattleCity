d528b06fc86ea1fdec7d436cd76f0d3434aed508
PillOfSpace <ennokey008@gmail.com>
2023-04-20 18:49:31 +0300

    Added ResourceManager

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 #include<glad/glad.h>
 #include <GLFW/glfw3.h>
+#include"Render/ShaderProgram.h"
+#include"Resources/ResourceManager.h"
+
+GLfloat point[] = {
+    0.0f,  0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+   -0.5f, -0.5f, 0.0f,
+};
+GLfloat colors[] = {
+    1.0f,  0.0f, 0.0f,
+    0.0f,  1.0f, 0.0f,
+    0.0f,  0.0f, 1.0f,
+};
+
+
 
 int g_WindowSizeX = 640;
 int g_WindowSizeY = 480;
@@ -8,18 +23,19 @@ int g_WindowSizeY = 480;
 void glfwWindowSizeCallback(GLFWwindow* pWindow, int wight, int height) {
     g_WindowSizeX = wight;
     g_WindowSizeY = height;
-    glViewport(0, 0, g_WindowSizeX, g_WindowSizeY); //Показываем в какой области мы хотим рисовать 
+    glViewport(0, 0, g_WindowSizeX, g_WindowSizeY); //Show you in which area we wnat to draw
 }
-//закрытие окна по нажатию клавиши escape
+//Close window to press escape
 void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
-        glfwSetWindowShouldClose(pWindow, GL_TRUE);//указываем GL что мы закрываем программу
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);//Show GL that we are closing window
     }
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
+    
     GLFWwindow* pWindow;
 
     /* Initialize the library */
@@ -28,10 +44,10 @@ int main(void)
         std::cout << "glfwInit failed!" << std::endl;
         return -1;
     }
-    //Указываю версию, если она меньше 4 и 6, то окно не запустится
+    //Pointing the version, if it's smaller 4 и 6, the window won't start
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    //использую "кор про файл"
+    //used core pro file
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
@@ -47,7 +63,7 @@ int main(void)
     glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
     glfwSetKeyCallback(pWindow, glfwKeyCallback);
 
-    /* делаем это окно текущим */
+    /* Making this window current */
     glfwMakeContextCurrent(pWindow);
 
     if (!gladLoadGL())
@@ -56,26 +72,64 @@ int main(void)
         return -1;
     }
 
-    std::cout << "Render: " << glGetString(GL_RENDERER) << std::endl; // показывает название видеокарты
-    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl; //показыввает версию OpenGL
-    std::cout << "OpenJL" << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Render: " << glGetString(GL_RENDERER) << std::endl; // Show the name of the video card
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl; //Show the version OpenGL
+    
 
-    glClearColor(0, 1, 0, 1);
+    glClearColor(1, 1, 0, 1);
 
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(pWindow))
+    
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        ResourceManager resourceManager(argv[0]);
+        auto pDefaultShaderProgram = resourceManager.loadShaders("DefaultShader", "res/shaders/vertex.txt", "res/shaders/fragment.txt");
+        if (!pDefaultShaderProgram)
+        {
+            std::cerr << "Can't create shader program: " << "DefaultShader" << std::endl;
+            return -1;
+        }
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(pWindow);
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        GLuint points_vbo = 0;
+        glGenBuffers(1, &points_vbo);//generate
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo); //connecting and current
+        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);// Filling in position on the video card
+
+        GLuint colors_vbo = 0;
+        glGenBuffers(1, &colors_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+        GLuint vao = 0;
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+
+        glEnableVertexAttribArray(0); //Enable position shader num 0
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);//Linking data
+
+        glEnableVertexAttribArray(1); //Enable position shader num 0
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);//Linking data
+
+
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(pWindow))
+        {
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            pDefaultShaderProgram->use();
+            glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(pWindow);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
     }
-
     glfwTerminate();
     return 0;
 }
